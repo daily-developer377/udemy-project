a6cf34feebf333af90631c73f5af507fcc40c643
Nandhu sunil <nandhusunil377@gmail.com>
2023-04-23 16:58:12 +0530

    added review schema

--- models/campground.js
+++ models/campground.js
@@ -6,6 +6,12 @@ const CampgroundSchema = new Schema({
   price: Number,
   description: String,
   location: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
