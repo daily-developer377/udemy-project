a53355978c8fc2ebac5b10ca2fdee47a2c58560d
Nandhu sunil <nandhusunil377@gmail.com>
2023-05-16 01:22:57 +0530

    added delete middle ware

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
+const { campgroundSchema } = require("../schemas");
 const Schema = mongoose.Schema;
+const Review = require("./review");
 const CampgroundSchema = new Schema({
   title: String,
   image: String,
@@ -12,6 +14,16 @@ const CampgroundSchema = new Schema({
       ref: "Review",
     },
   ],
+});
+
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
